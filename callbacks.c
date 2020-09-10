bffbd5bf53a2841d70a0c751424d31713bf7323e
Dantevg <mail@dantevg.nl>
2020-09-10 14:42:32 +0200

    Only execute when Lua didn't error

--- callbacks.c
+++ callbacks.c
@@ -13,19 +13,19 @@ int main(){
 	if(luaL_loadfile(L, "callbacks.lua") == LUA_OK){
 		if(lua_pcall(L, 0, 0, 0) == LUA_OK){
 			printf("[C] Code executed successfully\n");
+			
+			// Execute callbacks
+			for(int i = 0; i < 5; i++){
+				sleep(1);
+				lua_getglobal(L, "oncustomevent");
+				if(!lua_isfunction(L, -1)){
+					printf("[C] oncustomevent is not a function\n");
+				}
+				lua_pushinteger(L, i);
+				lua_pcall(L, 1, 0, 0);
+				printf("[C] oncustomevent called\n");
+			}
 		}
-	}
-	
-	// Execute callbacks
-	for(int i = 0; i < 5; i++){
-		sleep(1);
-		lua_getglobal(L, "oncustomevent");
-		if(!lua_isfunction(L, -1)){
-			printf("[C] oncustomevent is not a function\n");
-		}
-		lua_pushinteger(L, i);
-		lua_pcall(L, 1, 0, 0);
-		printf("[C] oncustomevent called\n");
 	}
 	
 	lua_close(L);
