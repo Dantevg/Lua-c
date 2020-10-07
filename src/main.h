8fa4072810b66ce2b69c3b14c7abd26dc7113b0d
Dantevg <dantevg006@gmail.com>
2020-10-07 16:45:47 +0200

    Add event name to callback struct

--- src/main.h
+++ src/main.h
@@ -1,12 +1,13 @@
-typedef struct {
+typedef struct Timer {
 	int fn;     // The function id in the Lua registry
 	int delay;  // The delay in ms
 	int repeat; // 1 = repeat, 0 = don't repeat
 } Timer;
 
-typedef struct {
-	int fn;     // The function id in the Lua registry
-	int data;   // Optional extra data in the Lua registry
+typedef struct Callback {
+	int fn;            // The function id in the Lua registry
+	int data;          // Optional extra data in the Lua registry
+	const char *event; // The event name
 } Callback;
 
 // Store the event names corresponding to the SDL2 event enum
