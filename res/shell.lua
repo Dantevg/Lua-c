7a5250afdd25f7648f73b8756c176d993a0a3036
Dantevg <dantevg006@gmail.com>
2020-10-20 16:44:46 +0200

    Add shell-like test

---
+++ res/shell.lua
@@ -0,0 +1,40 @@
+local event = require "event"
+local screen = require "screen"
+
+screen.loadFont("res/poly4x3-r_meta.lua")
+
+local input = ""
+local x, y = 0, 0
+
+function onkeydown(_, key)
+	if key == "backspace" and x > 0 then
+		input = input:sub(1, -2)
+		x = x-4
+		screen.colour(0)
+		screen.rect(x, y, 4, 5, true)
+		screen.colour(255)
+	elseif key == "return" then
+		input = ""
+		y = y + 6
+		x = 0
+	end
+end
+
+function oninput(_, text)
+	input = input .. text
+	screen.write(text, x, y)
+	x = x+4
+end
+
+function draw()
+	-- screen.clear()
+	
+	-- screen.write(input, 0, y)
+	
+	screen.present()
+end
+
+event.on("kb.down", onkeydown)
+event.on("kb.input", oninput)
+event.on("screen.resize", screen.resize)
+event.addTimer(20, draw, true)
