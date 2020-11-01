73a65a14dfc946f6a422364e244228aedd683231
Dantevg <dantevg006@gmail.com>
2020-11-01 16:45:50 +0100

    Small thread testing changes

--- res/test/thread.lua
+++ res/test/thread.lua
@@ -1,19 +1,24 @@
 local thread = require "thread"
 
+local a = 1
+
 local function threadfunction(n)
-	print("in thread "..n.."!")
+	print("in thread "..n..", a = "..a.."!")
+	a = a+1
 	
-	return 10, 20, 30
+	return 10, 20
 end
 
 local threads = {}
 
-for i = 1, 10 do
+for i = 1, 5 do
 	table.insert( threads, thread.new("Threadfunction"..i, threadfunction, i) )
 end
 
-for i = 1, 10 do
+for i = 1, 5 do
 	print(thread.wait(threads[i]))
 end
+
+print("in main, a = "..a.."!")
 
 return false
