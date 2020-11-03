12818287d5db6b2f33286231118298f30b4cfabc
Dantevg <dantevg006@gmail.com>
2020-11-03 16:48:59 +0100

    Update Lua test file

--- res/shell.lua
+++ res/shell.lua
@@ -1,7 +1,8 @@
+local window = require "SDLWindow"
 local event = require "event"
-local screen = require "screen"
 
-screen.loadFont("poly4x3-r_meta.lua")
+local screen = window.new()
+screen:loadFont("poly4x3-r_meta.lua")
 
 local input = ""
 local x, y = 0, 0
@@ -10,9 +11,9 @@ function onkeydown(_, key)
 	if key == "backspace" and x > 0 then
 		input = input:sub(1, -2)
 		x = x-4
-		screen.colour(0)
-		screen.rect(x, y, 4, 5, true)
-		screen.colour(255)
+		screen:colour(0)
+		screen:rect(x, y, 4, 5, true)
+		screen:colour(255)
 	elseif key == "return" then
 		input = ""
 		y = y + 6
@@ -22,19 +23,19 @@ end
 
 function oninput(_, text)
 	input = input .. text
-	screen.write(text, x, y)
+	screen:write(text, x, y)
 	x = x+4
 end
 
 function draw()
-	-- screen.clear()
+	-- screen:clear()
 	
-	-- screen.write(input, 0, y)
+	-- screen:write(input, 0, y)
 	
-	screen.present()
+	screen:present()
 end
 
 event.on("kb.down", onkeydown)
 event.on("kb.input", oninput)
-event.on("screen.resize", screen.resize)
+event.on("screen.resize", function(...) return screen:resize(...) end)
 event.addTimer(20, draw, true)
