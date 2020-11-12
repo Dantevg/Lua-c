0d2a9398d351875faa6f9e395c981aa0d51d0734
Dantevg <dantevg006@gmail.com>
2020-11-12 19:48:08 +0100

    Add command-line usage

--- src/main.c
+++ src/main.c
@@ -16,6 +16,15 @@
 	#define SO_EXT "so"
 #endif
 
+void print_usage(){
+	printf("Usage: Lua-C project [options] [file [args]]\n");
+	printf("Execute FILE, or the default boot file\n\n");
+	printf("Options:\n" );
+	printf("  -v, --version\tprint version\n");
+	printf("  -h, --help\tprint this help message\n");
+	printf("  -\t\tstop handling options and execute stdin\n");
+}
+
 // Callback to quit program as soon as possible upon user request
 int quit_callback(void *userdata, SDL_Event *event){
 	if(event->type == SDL_QUIT){
@@ -51,20 +60,32 @@ int loop(lua_State *L){
 
 int main(int argc, char *argv[]){
 	char *file = "res/main.lua";
+	int lua_arg_start = argc;
 	
 	/* Parse command-line arguments */
 	for(int i = 1; i < argc; i++){
 		if(strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0){
 			/* Print version and return */
-			printf("%s\n", VERSION);
+			printf("Lua-C project " VERSION "\n");
+			return 0;
+		}else if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0){
+			/* Print usage and return */
+			print_usage();
 			return 0;
 		}else if(strcmp(argv[i], "-") == 0){
 			/* Execute stdin */
 			file = NULL;
 			break;
+		}else if(argv[i][0] == '-'){
+			/* Unrecognised command line option */
+			printf("Unrecognised option: %s\n", argv[i]);
+			print_usage();
+			return -1;
 		}else{
 			/* Execute file */
 			file = argv[i];
+			lua_arg_start = i+1;
+			break;
 		}
 	}
 	
@@ -83,12 +104,12 @@ int main(int argc, char *argv[]){
 	SDL_AddEventWatch(quit_callback, L);
 	
 	/* Set cpath and path */
-	if(luaL_dostring(L, "package.cpath = package.cpath..';../bin/?." SO_EXT "'")){
+	if(luaL_dostring(L, "package.cpath = package.cpath..';./bin/?." SO_EXT "'")){
 		fprintf(stderr, "[C] Could not set package.cpath: %s\n", lua_tostring(L, -1));
 	}
-	// if(luaL_dostring(L, "package.path = package.path..';../res/?.lua'")){
-	// 	fprintf(stderr, "[C] Could not set package.path: %s\n", lua_tostring(L, -1));
-	// }
+	if(luaL_dostring(L, "package.path = package.path..';./res/lib/?.lua'")){
+		fprintf(stderr, "[C] Could not set package.path: %s\n", lua_tostring(L, -1));
+	}
 	
 	/* Register callbacks table */
 	lua_newtable(L); // stack: {tbl}
@@ -98,8 +119,12 @@ int main(int argc, char *argv[]){
 	
 	/* Load main file */
 	if(luaL_loadfile(L, file) == LUA_OK){
-		chdir("res");
-		if(lua_pcall(L, 0, 1, 0) == LUA_OK){
+		// chdir("res");
+		/* Push lua args */
+		for(int i = lua_arg_start; i < argc; i++){
+			lua_pushstring(L, argv[i]);
+		}
+		if(lua_pcall(L, argc-lua_arg_start, 1, 0) == LUA_OK){
 			// Immediately stop execution when main chunk returns false
 			if(lua_isboolean(L, -1) && lua_toboolean(L, -1) == 0){
 				return 0;
