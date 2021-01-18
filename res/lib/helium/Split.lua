1a4881940e61f90bfa120e5d1a3b4bb86e18ccc9
Dantevg <dantevg006@gmail.com>
2021-01-18 18:52:32 +0100

    Add split node
    TODO: fix usage of luaL_tointeger as it fails on non-integer inputs
    (instead use luaL_checknumber and lua_numbertointeger)

---
+++ res/lib/helium/Split.lua
@@ -0,0 +1,83 @@
+local Node = require "helium.Node"
+local Box = require "helium.Box"
+local Autopos = require "helium.Autopos"
+local Autosize = require "helium.Autosize"
+
+local Split = {}
+Split.__index = Split
+
+function Split.new(x, y, w, h, split)
+	local self = Box(x, y, w, h)
+	table.insert(self.tags, 1, "Split")
+	self.split = split
+	return setmetatable(self, Split)
+end
+
+function Split:Split() return self.split or 0.5 end
+
+function Split:drawself(canvas) end -- Don't draw self
+
+function Split:__tostring()
+	return string.format("Split @ (%d,%d) %dx%d %d%%", self.x, self.y, self.w, self.h, self.split*100)
+end
+
+
+
+Split.hor = {}
+Split.hor.__index = Split.hor
+
+function Split.hor.new(x, y, w, h, split)
+	local self = Split(x, y, w, h, split)
+	return setmetatable(self, Split.hor)
+end
+
+function Split.hor:insert(node, i)
+	Node.insert(self, node, i)
+	local n = #self.nodes
+	node.X = Autopos.hor.x(node)
+	node.Y = Autopos.hor.y(node)
+	node.W = function(el)
+		-- TODO: Remove math.floor
+		return math.floor( self.inner:W() * (n >= 2 and 1-self:Split() or self:Split()) - self.padding/2 )
+	end
+	node.H = Autosize.FitParent.h(node)
+end
+
+setmetatable(Split.hor, {
+	__index = Split,
+	__call = function(_, ...) return Split.hor.new(...) end,
+})
+
+
+
+Split.vert = {}
+Split.vert.__index = Split.vert
+
+function Split.vert.new(x, y, w, h, split)
+	local self = Split(x, y, w, h, split)
+	return setmetatable(self, Split.vert)
+end
+
+function Split.vert:insert(node, i)
+	Node.insert(self, node, i)
+	local n = #self.nodes
+	node.X = Autopos.vert.x(node)
+	node.Y = Autopos.vert.y(node)
+	node.W = Autosize.FitParent.w(node)
+	node.H = function(el)
+		-- TODO: Remove math.floor
+		return math.floor( self.inner:H() * (n >= 2 and 1-self:Split() or self:Split()) - self.padding/2 )
+	end
+end
+
+setmetatable(Split.vert, {
+	__index = Split,
+	__call = function(_, ...) return Split.vert.new(...) end,
+})
+
+
+
+return setmetatable(Split, {
+	__index = Box,
+	__call = function(_, ...) return Split.new(...) end,
+})
