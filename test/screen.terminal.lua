799a190224410faa3df95644edddbf93d20b3cc2
Dantevg <dantevg006@gmail.com>
2021-02-05 16:58:45 +0100

    Add terminal screen event test

--- test/screen.terminal.lua
+++ test/screen.terminal.lua
@@ -1,8 +1,20 @@
 local screen = require("screen.terminal").new()
-require "event"
+local event = require "event"
 
-screen:clear()
-screen:write("Hello, World!", 10, 5)
-screen:pixel(9, 4)
-screen:rect(10, 7, 20, 10, true)
-screen:present()
+local lastKey = 0
+
+function draw()
+	screen:clear()
+	screen:write(lastKey, 5, 5)
+	screen:write("Hello, World!", 10, 5)
+	screen:pixel(9, 4)
+	screen:rect(10, 7, 20, 10, true)
+	screen:present()
+end
+
+function kb(key)
+	lastKey = key
+end
+
+event.addTimer(100, draw, true)
+event.on("kb", "down", kb)
