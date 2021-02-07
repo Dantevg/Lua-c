78d612c2f2c97622e52f70b67ca47bed6bf4b773
Dantevg <dantevg006@gmail.com>
2021-02-07 16:27:11 +0100

    Simplify to use filewatch lib

--- res/filewatch.lua
+++ res/filewatch.lua
@@ -1,38 +1,7 @@
 local event = require "event"
-local lfs = require "lfs"
-
-local screen = require "screen.terminal"
-local s = screen.new()
+local filewatch = require "filewatch"
 
 local args = {...}
-local file = args[1]
-if not file then error("No file input given") end
+if not args[1] then error("No file input given") end
 
-local delay = args[2] and tonumber(args[2]) or 200
-
-local timestamp
-
-function change(newtime)
-	timestamp = newtime
-	s:clear()
-	local f = io.open(file)
-	if not f then error("Could not read file") end
-	local y = 0
-	for line in f:lines() do
-		s:write(line, 0, y)
-		y = y+1
-	end
-	f:close()
-	s:present()
-end
-
-function watch()
-	local newtime = lfs.attributes(file, "modification")
-	if not newtime then error("Could not get file information") end
-	if timestamp ~= newtime then
-		change(newtime)
-	end
-end
-
-event.addTimer(delay, watch, true)
-change(lfs.attributes(file, "modification"))
+event.on("filewatch", filewatch.watch(args[1]), print)
