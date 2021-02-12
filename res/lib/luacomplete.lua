be56d68fb0ce58bdde8614a7c3d7b57049796dc5
Dantevg <dantevg006@gmail.com>
2021-02-12 18:06:07 +0100

    Add luacomplete docs, rename internally

--- res/lib/luacomplete.lua
+++ res/lib/luacomplete.lua
@@ -1,4 +1,14 @@
-local autocomplete = {}
+--[[--
+	
+	Lua syntax autocompletions. Most important functions are @{complete} and @{hint}.
+	
+	@module luacomplete
+	@author RedPolygon
+	@license MIT
+	
+]]--
+
+local luacomplete = {}
 
 
 
@@ -21,37 +31,57 @@ end
 
 -- FINDING
 
-function autocomplete.findGlobal(input, env)
+--- Find a global.
+-- @tparam string input
+-- @tparam[opt] table env
+-- @return result
+function luacomplete.findGlobal(input, env)
 	local match = input:match("([%a_][%w_]*)$")
 	return (env or _G)[match]
 end
 
-function autocomplete.findKeyBracket(input)
+--- Find a key in bracket-style.
+-- @tparam string input
+-- @return value
+function luacomplete.findKeyBracket(input)
 	local from, _, match = input:find("%['([^']*)'%]$")
 	if not from then
 		from, _, match = input:find('%["([^"]*)"%]$')
 	end
 	if not match then return end
-	return (autocomplete.find(input:sub(1, from-1)) or {})[match]
+	return (luacomplete.find(input:sub(1, from-1)) or {})[match]
 end
 
-function autocomplete.findKeyDot(input)
+--- Find a key in dot-style.
+-- @tparam string input
+-- @return value
+function luacomplete.findKeyDot(input)
 	local from, _, match = input:find("%.([%a_][%w_]*)$")
 	if not match then return end
-	return (autocomplete.find(input:sub(1, from-1)) or {})[match]
+	return (luacomplete.find(input:sub(1, from-1)) or {})[match]
 end
 
-function autocomplete.find(input)
-	return autocomplete.findKeyBracket(input)
-		or autocomplete.findKeyDot(input)
-		or autocomplete.findGlobal(input)
+--- Get a value.
+-- @tparam string input
+-- @return value
+function luacomplete.find(input)
+	return luacomplete.findKeyBracket(input)
+		or luacomplete.findKeyDot(input)
+		or luacomplete.findGlobal(input)
 end
 
 
 
 -- AUTOCOMPLETION
 
-function autocomplete.completeListK(input, list, after)
+--- Find a completion in the keys of a table.
+-- When the table has a metatable with a table field __index,
+-- that will also be traversed, recursively.
+-- @tparam string input
+-- @tparam table list
+-- @tparam[opt] string after will be placed after every completion
+-- @treturn table completions
+function luacomplete.completeListK(input, list, after)
 	local completions = {}
 	if type(list) == "table" then
 		for name, _ in pairs(list) do
@@ -64,13 +94,18 @@ function autocomplete.completeListK(input, list, after)
 	-- Traverse __index
 	local mt = getmetatable(list)
 	if type(mt) == "table" and type(mt.__index) == "table" and mt.__index ~= list then
-		concat(completions, autocomplete.completeListK(input, mt.__index, after))
+		concat(completions, luacomplete.completeListK(input, mt.__index, after))
 	end
 	
 	return completions
 end
 
-function autocomplete.completeListV(input, list, after)
+--- Find a completion in a table of values.
+-- @tparam string input
+-- @tparam table list
+-- @tparam[opt] string after will be placed after every completion
+-- @treturn table completions
+function luacomplete.completeListV(input, list, after)
 	local completions = {}
 	for _, name in ipairs(list or {}) do
 		if name:sub(1, #input) == input then
@@ -80,55 +115,75 @@ function autocomplete.completeListV(input, list, after)
 	return completions
 end
 
-autocomplete.keywords = {
+--- List of Lua 5.3 keywords.
+luacomplete.keywords = {
 	"and", "break", "do", "else", "elseif",
 	"end", "false", "for", "function", "if",
 	"in", "local", "nil", "not", "or",
 	"repeat", "return", "then", "true", "until", "while",
 }
 
-function autocomplete.completeKeyword(input)
+--- Try to complete a keyword.
+-- @tparam string input
+-- @treturn table completions
+function luacomplete.completeKeyword(input)
 	local match = input:match("[^%.:%w](%w+)$") or input:match("^(%w+)$")
 	if not match then return {} end
-	return autocomplete.completeListV(match, autocomplete.keywords)
+	return luacomplete.completeListV(match, luacomplete.keywords)
 end
 
-function autocomplete.completeGlobal(input, env)
+--- Try to complete a global.
+-- @tparam string input
+-- @tparam[opt=_G] table env
+-- @treturn table completions
+function luacomplete.completeGlobal(input, env)
 	local match = input:match("[^%.:%a_]([%a_][%w_]*)$") or input:match("^([%a_][%w_]*)$")
 	if not match then return {} end
-	return autocomplete.completeListK(match, env or _G)
+	return luacomplete.completeListK(match, env or _G)
 end
 
-function autocomplete.completeKeyBracket(input)
+--- Try to complete a table key in bracket-style.
+-- @tparam string input
+-- @treturn table completions
+function luacomplete.completeKeyBracket(input)
 	local after, from, _, match = "']", input:find("%['([^']*)$")
 	if not match then
 		after, from, _, match = '"]', input:find('%["([^"]*)$')
 	end
 	if not match then return end
-	return autocomplete.completeListK(match,
-		autocomplete.find(input:sub(1, from-1)), after)
+	return luacomplete.completeListK(match,
+		luacomplete.find(input:sub(1, from-1)), after)
 end
 
-function autocomplete.completeKeyDot(input)
+--- Try to complete a table key in dot-style.
+-- @tparam string input
+-- @treturn table completions
+function luacomplete.completeKeyDot(input)
 	local from, _, match = input:find("%.([%a_][%w_]*)$")
 	if not match then
 		from, _, match = input:find("%.$")
 		if from then match = "" end
 	end
 	if not match then return end
-	return autocomplete.completeListK(match, autocomplete.find(input:sub(1, from-1)))
+	return luacomplete.completeListK(match, luacomplete.find(input:sub(1, from-1)))
 end
 
-function autocomplete.completeKey(input)
-	return autocomplete.completeKeyBracket(input)
-		or autocomplete.completeKeyDot(input)
+--- Try to complete a table key in both bracket and dot style.
+-- @tparam string input
+-- @treturn table completions
+function luacomplete.completeKey(input)
+	return luacomplete.completeKeyBracket(input)
+		or luacomplete.completeKeyDot(input)
 		or {}
 end
 
-function autocomplete.complete(input)
-	local key = autocomplete.completeKey(input)
-	local global = autocomplete.completeGlobal(input)
-	local keyword = autocomplete.completeKeyword(input)
+--- Try to complete any input (table key, global and keyword).
+-- @tparam string input
+-- @treturn table completions (sorted)
+function luacomplete.complete(input)
+	local key = luacomplete.completeKey(input)
+	local global = luacomplete.completeGlobal(input)
+	local keyword = luacomplete.completeKeyword(input)
 	local completions = concat(key, global, keyword)
 	table.sort(completions)
 	return completions
@@ -138,7 +193,11 @@ end
 
 -- HINTING
 
-function autocomplete.hintFunction(completion, f)
+--- Get hint for a function.
+-- @tparam string completion
+-- @tparam function f
+-- @treturn string hint
+function luacomplete.hintFunction(completion, f)
 	local d = debug.getinfo(f)
 	local args = {}
 	for i = 1, d.nparams do
@@ -148,30 +207,36 @@ function autocomplete.hintFunction(completion, f)
 	return completion.."("..table.concat(args, ", ")..")"
 end
 
-function autocomplete.hints(input)
-	local completions = autocomplete.complete(input)
-	for i, completion in ipairs(completions) do
-		local value = autocomplete.find(input..completion)
+--- Get hints for an input.
+-- @tparam string input
+-- @treturn table hints
+function luacomplete.hints(input)
+	local hints = luacomplete.complete(input)
+	for i, completion in ipairs(hints) do
+		local value = luacomplete.find(input..completion)
 		if type(value) == "function" then
-			completions[i] = autocomplete.hintFunction(completion, value)
+			hints[i] = luacomplete.hintFunction(completion, value)
 		elseif type(value) == "table" then
-			completions[i] = completion.."."
+			hints[i] = completion.."."
 		end
 	end
-	return completions
+	return hints
 end
 
-function autocomplete.hint(input)
-	local completion = autocomplete.complete(input)[1]
-	if not completion then return end
+--- Get hint for an input.
+-- @tparam string input
+-- @treturn string hint
+function luacomplete.hint(input)
+	local hint = luacomplete.complete(input)[1]
+	if not hint then return end
 	
-	local value = autocomplete.find(input..completion)
+	local value = luacomplete.find(input..hint)
 	if type(value) == "function" then
-		return autocomplete.hintFunction(completion, value)
+		return luacomplete.hintFunction(hint, value)
 	elseif type(value) == "table" then
-		return completion.."."
+		return hint.."."
 	else
-		return completion
+		return hint
 	end
 end
 
@@ -179,8 +244,8 @@ end
 
 -- RETURN
 
-function autocomplete.__call(_, ...)
-	return autocomplete.complete(...)
+function luacomplete.__call(_, ...)
+	return luacomplete.complete(...)
 end
 
-return setmetatable(autocomplete, autocomplete)
+return setmetatable(luacomplete, luacomplete)
