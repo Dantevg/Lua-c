9a79578076f842520af840438d96580308a040d8
Dantevg <dantevg006@gmail.com>
2021-02-16 18:36:29 +0100

    Implement more (also adapt from Lua's liolib.c)

--- src/fs.c
+++ src/fs.c
@@ -1,6 +1,9 @@
 /***
  * The `fs` module provides access to MoonBox's filesystem
  * @module fs
+ * 
+ * Parts of this code are taken from Lua's liolib.c:
+ * https://github.com/lua/lua/blob/master/liolib.c
  */
 
 #include <stdlib.h>
@@ -34,16 +37,32 @@ int fs_match_base(const char *a, const char *b){
 	return 1;
 }
 
+FS_File *fs_getfile(lua_State *L){
+	FS_File *file = luaL_checkudata(L, 1, "File");
+	if(!file->opened) luaL_error(L, "attempt to use a closed file");
+	return file;
+}
+
 /* Lua API definitions */
 
 int fs_mount_l(lua_State *L){
 	char *path = copy_str(L, 1);
 	if(lua_isuserdata(L, 2)){
 		fs_mount(L, path, lua_touserdata(L, 2));
+		lua_pushboolean(L, 1);
 	}else{
-		// FS *fs = malloc(sizeof(FS));
+		lua_pushboolean(L, 0);
 	}
-	return 0;
+	return 1;
+}
+
+int fs_openwith(lua_State *L, const char *path, const char *mode, FS *fs){
+	FS_File *file = lua_newuserdata(L, sizeof(FS_File));
+	file->opened = 1;
+	file->fs = fs;
+	file->fd = fs->open(path, mode);
+	luaL_setmetatable(L, "File");
+	return (file->fd == NULL) ? luaL_fileresult(L, 0, path) : 1;
 }
 
 int fs_open(lua_State *L){
@@ -54,12 +73,7 @@ int fs_open(lua_State *L){
 	lua_pushnil(L);
 	while(lua_next(L, t) != 0){
 		if(fs_match_base(path, lua_tostring(L, -2))){
-			FS *fs = lua_touserdata(L, -1);
-			FS_File *file = lua_newuserdata(L, sizeof(FS_File));
-			file->fs = fs;
-			file->fd = fs->open(path, mode);
-			luaL_setmetatable(L, "File");
-			return 1;
+			return fs_openwith(L, path, mode, lua_touserdata(L, -1));
 		}
 		lua_pop(L, 1);
 	}
@@ -67,17 +81,24 @@ int fs_open(lua_State *L){
 }
 
 int fs_close(lua_State *L){
-	FS_File *file = luaL_checkudata(L, 1, "File");
+	FS_File *file = fs_getfile(L);
+	file->opened = 0;
 	file->fs->close(file->fd);
-	return 0;
+	lua_pushboolean(L, 1);
+	return 1;
 }
 
 int fs_flush(lua_State *L){
+	FS_File *file = fs_getfile(L);
+	return luaL_fileresult(L, file->fs->flush(file->fd) == 0, NULL);
+}
+
+int fs_lines(lua_State *L){
 	return 0;
 }
 
 int fs_read(lua_State *L){
-	FS_File *file = luaL_checkudata(L, 1, "File");
+	FS_File *file = fs_getfile(L);
 	int n = luaL_checkinteger(L, 2);
 	char *buffer = malloc(n * sizeof(char));
 	int n_read = file->fs->read(file->fd, buffer, n);
@@ -90,19 +111,42 @@ int fs_read(lua_State *L){
 	return 1;
 }
 
-int fs_getc(lua_State *L){
-	return 0;
-}
-
+// Adapted from https://github.com/lua/lua/blob/c03c527fd207b4ad8f5a8e0f4f2c176bd227c979/liolib.c#L696
 int fs_seek(lua_State *L){
-	return 0;
+	static const int modes[] = {SEEK_SET, SEEK_CUR, SEEK_END};
+	static const char *const modenames[] = {"set", "cur", "end", NULL};
+	FS_File *file = fs_getfile(L);
+	int whence = luaL_checkoption(L, 2, "cur", modenames);
+	lua_Integer p3 = luaL_optinteger(L, 3, 0);
+	long offset = (long)p3;
+	luaL_argcheck(L, (lua_Integer)offset == p3, 3, "not an integer in proper range");
+	int result = file->fs->seek(file->fd, offset, modes[whence]);
+	if(result != 0){
+		return luaL_fileresult(L, 0, NULL);
+	}else{
+		lua_pushinteger(L, file->fs->tell(file->fd));
+		return 1;
+	}
 }
 
+// Adapted from https://github.com/lua/lua/blob/c03c527fd207b4ad8f5a8e0f4f2c176bd227c979/liolib.c#L715
 int fs_setvbuf(lua_State *L){
-	return 0;
+	static const int modes[] = {_IONBF, _IOFBF, _IOLBF};
+	static const char *const modenames[] = {"no", "full", "line", NULL};
+	FS_File *file = fs_getfile(L);
+	int mode = luaL_checkoption(L, 2, NULL, modenames);
+	size_t size = luaL_optinteger(L, 3, LUAL_BUFFERSIZE);
+	int res = file->fs->setvbuf(file->fd, modes[mode], size);
+	return luaL_fileresult(L, res == 0, NULL);
 }
 
 int fs_write(lua_State *L){
+	return 0;
+}
+
+int fs__gc(lua_State *L){
+	FS_File *file = luaL_checkudata(L, 1, "File");
+	if(file->opened) file->fs->close(file->fd);
 	return 0;
 }
 
@@ -112,14 +156,19 @@ static const struct luaL_Reg fs_f[] = {
 	{NULL, NULL}
 };
 
-static const struct luaL_Reg fs_m[] = {
+static const struct luaL_Reg fs_file_f[] = {
 	{"close", fs_close},
 	{"flush", fs_flush},
+	{"lines", fs_lines},
 	{"read", fs_read},
-	{"getc", fs_getc},
 	{"seek", fs_seek},
 	{"setvbuf", fs_setvbuf},
 	{"write", fs_write},
+	{NULL, NULL}
+};
+
+static const struct luaL_Reg fs_file_m[] = {
+	{"__gc", fs__gc},
 	{NULL, NULL}
 };
 
@@ -127,8 +176,9 @@ LUAMOD_API int luaopen_fs(lua_State *L){
 	luaL_newlib(L, fs_f);
 	
 	luaL_newmetatable(L, "File");
-	luaL_newlib(L, fs_m);
+	luaL_newlib(L, fs_file_f);
 	lua_setfield(L, -2, "__index");
+	luaL_setfuncs(L, fs_file_m, 0);
 	lua_pop(L, 1);
 	
 	lua_newtable(L);
