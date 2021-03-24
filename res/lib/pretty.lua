c26f47115283cfb1603cd48e2555a1e954edf18b
Dantevg <dantevg006@gmail.com>
2021-03-24 14:24:31 +0100

    Split prettyprint from lua commandline

---
+++ res/lib/pretty.lua
@@ -0,0 +1,97 @@
+local tc = require "prequire" "terminalcolours" -- Can do without colours
+if not tc then
+	-- So that the program will still run without terminalcolours
+	tc = setmetatable(
+		{fg = {}, bg = {}, cursor = {}},
+		{__call = function() return "" end}
+	)
+end
+
+local pretty = {}
+
+pretty.reset = {tc.reset, tc.bright}
+
+local reset = pretty.reset
+local function nop(x) return tc(reset)..tostring(x) end
+function pretty.special(x)
+	return string.format("%s[%s]",
+		tc(tc.bright, tc.fg.magenta), tostring(x))
+end
+local function prettyprint(x, long)
+	local t = type(x)
+	
+	if pretty[t] and not rawget(getmetatable(x) or {}, "__tostring") then
+		return pretty[t](x, long)..tc(reset)
+	else
+		return nop(x)..tc(reset)
+	end
+end
+local function prettykey(x)
+	if type(x) == "string" and x:match("^[%a_][%w_]*$") then
+		return tostring(x)
+	else
+		return "["..prettyprint(x).."]"
+	end
+end
+
+pretty["nil"] = function(x) return tc(tc.reset, tc.fg.grey)..tostring(x) end
+pretty["number"] = function(x) return tc(tc.reset, tc.fg.cyan)..tostring(x) end
+pretty["string"] = function(x) return tc(tc.reset, tc.fg.green)..'"'..x..'"' end
+pretty["boolean"] = function(x) return tc(tc.reset, tc.fg.yellow)..tostring(x) end
+pretty["table"] = function(x, long)
+	if type(x) ~= "table" and not rawget(getmetatable(x) or {}, "__pairs") then
+		return pretty.error("not a table")
+	end
+	if not long then return pretty.special(x) end
+	
+	local contents = {}
+	
+	if type(x) == "table" then
+		for _, v in ipairs(x) do
+			table.insert(contents, prettyprint(v))
+		end
+	end
+	
+	for k, v in pairs(x) do
+		if not contents[k] then
+			table.insert(contents, prettykey(k).." = "..prettyprint(v))
+		end
+	end
+	
+	return tc(reset).."{ "..table.concat(contents, ", ").." }"
+		..(getmetatable(x) and tc(tc.fg.grey).." + mt" or "")
+end
+pretty["function"] = function(x, long)
+	if type(x) ~= "function" then return pretty.error("not a function") end
+	local d = debug.getinfo(x, "S")
+	local filename = d.short_src:match("([^/]+)$")
+	local str = string.format("%s[%s @ %s:%d]",
+		tc(tc.bright, tc.fg.magenta), tostring(x), filename, d.linedefined)
+		
+	if not long or d.source:sub(1,1) ~= "@" or d.source == "@stdin" then
+		return str
+	end
+	
+	local file = io.open(d.source:sub(2))
+	local contents = {}
+	local i = 1
+	for line in file:lines() do
+		if i >= d.linedefined then
+			if i > d.linedefined then break end
+			table.insert(contents, line)
+		end
+		i = i+1
+	end
+	file:close()
+	return str..tc(reset).."\n"..table.concat(contents, "\n")
+end
+pretty["thread"] = pretty.special
+pretty["userdata"] = pretty.special
+
+pretty["error"] = function(x)
+	return tc(tc.bright, tc.fg.red)..tostring(x)
+end
+
+return setmetatable(pretty, {
+	__call = function(_, ...) return prettyprint(...) end,
+})
--- res/lua.lua
+++ res/lua.lua
@@ -1,6 +1,8 @@
-local terminal = require "prequire" "terminal" -- Advanced terminal features may not be available
-local tc = require "prequire" "terminalcolours" -- Can do without colours
-local autocomplete = require "prequire" "luacomplete" -- Don't *need* autocompletion
+local prequire = require "prequire"
+local terminal = prequire "terminal" -- Advanced terminal features may not be available
+local tc = prequire "terminalcolours" -- Can do without colours
+local autocomplete = prequire "luacomplete" -- Don't *need* autocompletion
+local pretty = prequire "pretty"
 
 local historyPath = "res/.luahistory.txt"
 
@@ -32,99 +34,25 @@ else
 	end
 end
 
-
-
-local prettyprint = {}
-
-local resultstyle = {tc.reset, tc.bright}
-local function nop(x) return tc(resultstyle)..tostring(x) end
-local function special(x)
-	return string.format("%s[%s]",
-		tc(tc.bright, tc.fg.magenta), tostring(x))
-end
-local function pretty(x, long)
-	local t = type(x)
-	
-	if prettyprint[t] and not rawget(getmetatable(x) or {}, "__tostring") then
-		return prettyprint[t](x, long)..tc(resultstyle)
-	else
-		return nop(x)..tc(resultstyle)
-	end
-end
-local function prettykey(x)
-	if type(x) == "string" and x:match("^[%a_][%w_]*$") then
-		return tostring(x)
-	else
-		return "["..pretty(x).."]"
-	end
+if not pretty then
+	pretty = setmetatable(
+		{reset = {}},
+		{
+			__call = function(_, s) return s end,
+			__index = function() return function(s) return s end end,
+		}
+	)
 end
 
-prettyprint["nil"] = function(x) return tc(tc.reset, tc.fg.grey)..tostring(x) end
-prettyprint["number"] = function(x) return tc(tc.reset, tc.fg.cyan)..tostring(x) end
-prettyprint["string"] = function(x) return tc(tc.reset, tc.fg.green)..'"'..x..'"' end
-prettyprint["boolean"] = function(x) return tc(tc.reset, tc.fg.yellow)..tostring(x) end
-prettyprint["table"] = function(x, long)
-	if type(x) ~= "table" and not rawget(getmetatable(x) or {}, "__pairs") then
-		return prettyprint.error("not a table")
-	end
-	if not long then return special(x) end
-	
-	local contents = {}
-	
-	if type(x) == "table" then
-		for _, v in ipairs(x) do
-			table.insert(contents, pretty(v))
-		end
-	end
-	
-	for k, v in pairs(x) do
-		if not contents[k] then
-			table.insert(contents, prettykey(k).." = "..pretty(v))
-		end
-	end
-	
-	return tc(resultstyle).."{ "..table.concat(contents, ", ").." }"
-		..(getmetatable(x) and tc(tc.fg.grey).." + mt" or "")
-end
-prettyprint["function"] = function(x, long)
-	if type(x) ~= "function" then return prettyprint.error("not a function") end
-	local d = debug.getinfo(x, "S")
-	local filename = d.short_src:match("([^/]+)$")
-	local str = string.format("%s[%s @ %s:%d]",
-		tc(tc.bright, tc.fg.magenta), tostring(x), filename, d.linedefined)
-		
-	if not long or d.source:sub(1,1) ~= "@" or d.source == "@stdin" then
-		return str
-	end
-	
-	local file = io.open(d.source:sub(2))
-	local contents = {}
-	local i = 1
-	for line in file:lines() do
-		if i >= d.linedefined then
-			if i > d.linedefined then break end
-			table.insert(contents, line)
-		end
-		i = i+1
-	end
-	file:close()
-	return str..tc(resultstyle).."\n"..table.concat(contents, "\n")
-end
-prettyprint["thread"] = special
-prettyprint["userdata"] = special
-
-prettyprint["error"] = function(x)
-	return tc(tc.bright, tc.fg.red)..tostring(x)
-end
-
-prettyprint["trace"] = function(x)
+-- Add trace prettyprint
+pretty.trace = function(x)
 	local args = {}
 	for _, arg in ipairs(x.args) do
 		table.insert(args, tc(tc.reset)..tostring(arg[1])..": "..pretty(arg[2]))
 	end
 	return string.format("%s(%s%s) %s",
-		x.name or x.source, table.concat(args, ", "), tc(resultstyle),
-		prettyprint["function"](x.func))
+		x.name or x.source, table.concat(args, ", "), tc(pretty.reset),
+		pretty["function"](x.func))
 end
 
 
@@ -134,19 +62,19 @@ local commands = {}
 function commands.table(arg)
 	local tbl = _G[arg]
 	if not tbl then return end
-	print(prettyprint.table(tbl, true))
+	print(pretty.table(tbl, true))
 end
 
 function commands.metatable(arg)
 	local mt = getmetatable(_G[arg])
 	if not mt then return end
-	print(prettyprint.table(mt, true))
+	print(pretty.table(mt, true))
 end
 
 commands["function"] = function(arg)
 	local fn = _G[arg]
 	if not fn then return end
-	print(prettyprint["function"](fn, true))
+	print(pretty["function"](fn, true))
 end
 
 function commands.require(arg)
@@ -156,7 +84,7 @@ function commands.require(arg)
 	if success then
 		_G[arg] = result
 	else
-		print(prettyprint.error(result))
+		print(pretty.error(result))
 	end
 end
 
@@ -182,7 +110,7 @@ function commands.complete(arg)
 	if not autocomplete then autocomplete = require "luacomplete" end
 	local completions = autocomplete(arg or "")
 	for _, completion in ipairs(completions) do
-		print(tc(tc.fg.grey)..arg..tc(resultstyle)..completion)
+		print(tc(tc.fg.grey)..arg..tc(pretty.reset)..completion)
 	end
 end
 
@@ -215,7 +143,7 @@ commands.q = commands.exit
 
 
 local function onerror(err)
-	print(debug.traceback(prettyprint.error(err), 2))
+	print(debug.traceback(pretty.error(err), 2))
 end
 
 local function result(success, ...)
@@ -237,10 +165,10 @@ local function result(success, ...)
 		if trace[i].type == "return" then
 			level = level-1
 		elseif trace[i].type == "call" then
-			print(tc(resultstyle)
+			print(tc(pretty.reset)
 				..string.rep(unicode and "\u{2502} " or "| ", level)
 				..(unicode and "\u{251c}\u{2574}" or "|-")
-				..prettyprint.trace(trace[i]))
+				..pretty.trace(trace[i]))
 			level = level+1
 		end
 	end
@@ -292,7 +220,7 @@ while true do
 		local fn, err = load("return "..input, "=stdin", "t")
 		if not fn then fn, err = multiline(input) end
 		if not fn then
-			print(prettyprint.error(err))
+			print(pretty.error(err))
 		else
 			trace = {}
 			if dotrace then debug.sethook(hook, "cr") end
