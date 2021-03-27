5cf289e981247e2bf91bfded481a8a51ee6494ed
Dantevg <dantevg006@gmail.com>
2021-03-27 20:38:31 +0100

    Add test for async

--- test/auto/safethread.lua
+++ test/auto/safethread.lua
@@ -2,16 +2,18 @@ local thread = require "safethread"
 
 do
 	-- Function argument and return value order
-	local t = thread.new(function(x, y) return x, y, x - y end, 42, 10)
-	local a, b, c = t:wait()
+	local t = thread.new()
+	local _, a, b, c = t:pcall(function(x, y) return x, y, x - y end, 42, 10)
 	assert(a == 42)
 	assert(b == 10)
 	assert(c == 32)
+	t:wait()
 end
 
 do
 	-- Function environment
-	local t = thread.new(function() x = 10 end)
+	local t = thread.new()
+	t:pcall(function() x = 10 end)
 	local y = 20
 	z = 30
 	local _, a, b, c = t:pcall(function() return x, y, z end)
@@ -23,7 +25,8 @@ end
 
 do
 	-- More environment stuff
-	local t = thread.new(function() x = 10 end)
+	local t = thread.new()
+	t:pcall(function() x = 10 end)
 	local y = 20
 	z = 30
 	local _, a, b, c = t:pcall(function()
@@ -41,7 +44,7 @@ end
 
 do
 	-- Recursive references
-	local t = thread.new(function() end)
+	local t = thread.new()
 	function f(tbl)
 		assert(tbl.t == tbl.t.t)
 		assert(tbl.t == tbl.f())
@@ -52,5 +55,13 @@ do
 	tbl.t = tbl
 	f(tbl)
 	f(select(2, t:pcall(f, tbl)))
+	t:wait()
+end
+
+do
+	-- Async functions
+	local t = thread.new()
+	t:pcall(function() x = 42 end)
+	t:async(function(y) return x - y end, 10)(function(a) assert(a == 32) end)
 	t:wait()
 end
